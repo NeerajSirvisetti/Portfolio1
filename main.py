4717370575b678494d7e28e15592919ca5daa2a4
NeerajSirvisetti <32873782+NeerajSirvisetti@users.noreply.github.com>
2022-06-27 17:33:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect
 import csv
+import os
 
 
 app = Flask(__name__)
@@ -40,4 +41,6 @@ def submit_form():
     else:
       return 'something went wrong. Try again!'
 
-app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
